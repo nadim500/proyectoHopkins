bfa658c939be9b131f592e176d444ea1264c5639
Duivan Mata <duivan5@gmail.com>
2016-02-08 19:08:03 -0500

    una parte de interfaz

--- server/boot/controller.js
+++ server/boot/controller.js
@@ -1,40 +1,54 @@
+module.exports = function(app) {
+
+    var router = app.loopback.Router();
+    var Cliente = app.models.Cliente;
+    var Distrito = app.models.Distrito;
+    var Pedido = app.models.Pedido;
+    var TipoServicio = app.models.TipoServicio;
+    var Trabajador = app.models.Trabajador;
+    var Vehiculo = app.models.Vehiculo;
+
+    router.get('/', function(req, res) {
+        res.send("Hola mundo");
+    });
+
+    router.get('/', function(req, res) {
+        res.redirect('login');
+    });
+
+    router.get('/login', function(req, res) {
+        res.render('login');
+    });
+
+    router.get('/homepage', function(req, res) {
+        res.render('homepage');
+    });
+
+    router.get('/login', function(req, res) {
+        res.render('login');
+    });
+
+    router.get('/principal', function(req, res) {
+        res.render('principal');
+    });
+
+    router.get('/clientePrincipal', function(req, res) {
+        res.render('clientePrincipal');
+    })
+
+    router.get('/clienteCrear', function(req, res) {
+        res.render('clienteCrear');
+    });
+
+    router.get('/pedidoCrear', function(req, res) {
+        res.render('pedidoCrear');
+    });
+
+    router.get('/signin', function(req, res) {
+        res.render('signin');
+    });
 
 
-module.exports = function(app){
 
-	var router = app.loopback.Router();
-	var Cliente = app.models.Cliente;
-	var Distrito = app.models.Distrito;
-	var Pedido = app.models.Pedido;
-	var TipoServicio = app.models.TipoServicio;
-	var Trabajador = app.models.Trabajador;
-	var Vehiculo = app.models.Vehiculo;
-
-	router.get('/',function(req,res){
-		res.send("Hola mundo");
-	});
-	
-	router.get('/',function(req,res){
-		res.redirect('login');	
-	});
-	
-	router.get('/login',function(req,res){
-		res.render('login');
-	});
-	
-	router.get('/homepage',function(req,res){
-		res.render('homepage');
-	})
-	
-	
-	router.get('/kewin',function(req,res){
-		var nombre = 'Kewin'
-		res.render('kewin',{
-			nombre:nombre
-		})
-	})
-
-
-
-	app.use(router);
+    app.use(router);
 }
