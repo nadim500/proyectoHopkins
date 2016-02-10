6f40021f6dadfd28baa73801e5b6c1ea3a89a6eb
Duivan Mata <duivan5@gmail.com>
2016-02-10 14:54:51 -0500

    pedido en bd listo

--- server/boot/controller.js
+++ server/boot/controller.js
@@ -2,7 +2,6 @@ module.exports = function(app) {
 
     var router = app.loopback.Router();
     var Cliente = app.models.Cliente;
-    var Distrito = app.models.Distrito;
     var Pedido = app.models.Pedido;
     var TipoServicio = app.models.TipoServicio;
     var Trabajador = app.models.Trabajador;
@@ -47,9 +46,12 @@ module.exports = function(app) {
     });
 
     router.post('/nuevoCliente', function(req, res) {
-        nombre = req.body.nombreCliente;
-        telefono = req.body.telefonoCliente;
-        direccion = req.body.direccionCliente;
+        var filtro = req.body.filtro;
+        console.log("--->", typeof(filtro));
+        console.log("filtro: ", filtro)
+        var nombre = req.body.nombreCliente;
+        var telefono = req.body.telefonoCliente;
+        var direccion = req.body.direccionCliente;
 
         var nuevoCliente = {
             nombre: nombre,
@@ -57,21 +59,28 @@ module.exports = function(app) {
             direccion_cliente: direccion
         };
 
-        Cliente.create(nuevoCliente, function(err, obj) {
+        Cliente.create(nuevoCliente, function(err, objCliente) {
             if (err) return res.sendStatus(404);
-            modo = true;
-            mostrarTitulo = "Nuevo Cliente";
-            mostrarMensaje = "El Cliente " + nuevoCliente.nombre + " fue creado con exito";
-            Cliente.find({}, function(err, objResult_cliente) {
-                if (err) return res.sendStatus(404);
-                return res.render('clientePrincipal', {
-                    objResult_cliente: objResult_cliente,
-                    modo: modo,
-                    mostrarMensaje: mostrarMensaje,
-                    mostrarTitulo: mostrarTitulo
+            if (filtro == "true") {
+                var modo = true;
+                var mostrarTitulo = "Nuevo Cliente";
+                var mostrarMensaje = "El Cliente " + nuevoCliente.nombre + " fue creado con exito";
+                Cliente.find({}, function(err, objResult_cliente) {
+                    if (err) return res.sendStatus(404);
+                    return res.render('clientePrincipal', {
+                        objResult_cliente: objResult_cliente,
+                        modo: modo,
+                        mostrarMensaje: mostrarMensaje,
+                        mostrarTitulo: mostrarTitulo
+                    });
                 });
-            });
+            } else {
+                return res.render('pedidoCrear', {
+                    objCliente: objCliente
+                });
+            }
         });
+
     });
 
     ///////////////////////PEDIDO////////////////////
@@ -87,23 +96,27 @@ module.exports = function(app) {
     router.get('/pedidoCrear', function(req, res) {
 
         res.render('pedidoCrear');
+
     });
 
     router.post('/nuevoPedido', function(req, res) {
         //FALTA IMPORTAR EL ID DEL CLIENTE
         //AÑADIR EL INCLUDE AL PEDIDO PARA RENDERIZAR
         //EL CLIENTE EN LA PAGINA PRINCIPAL DEL PEDIDO
-        numeroPaquete = req.body.paquetePedido;
-        etapaPedido = req.body.etapaPedido
-        costoPedido = req.body.costoPedido;
-        direccion = req.body.direccionPedido;
-        distrito = req.body.distritoPedido;
 
-        tipoServicio = req.body.tipoServicio;
-        formaPago = req.body.formaPago;
-        pagaPrimero = req.body.pagaPrimero;
+        var idCliente = req.body.idCliente;
+        console.log("idCliente----->",idCliente);
+        var numeroPaquete = req.body.paquetePedido;
+        var etapaPedido = req.body.etapaPedido
+        var costoPedido = req.body.costoPedido;
+        var direccion = req.body.direccionPedido;
+        var distrito = req.body.distritoPedido;
 
-        tipoVehiculo = req.body.tipoVehiculo;
+        var tipoServicio = req.body.tipoServicio;
+        var formaPago = req.body.formaPago;
+        var pagaPrimero = req.body.pagaPrimero;
+
+        var tipoVehiculo = req.body.tipoVehiculo;
 
         var nuevoServicio = {
             tipo_servicio: tipoServicio,
@@ -111,29 +124,37 @@ module.exports = function(app) {
             paga_primero: pagaPrimero
         };
 
-
-        TipoServicio.create(nuevoServicio, function(err, objResult_servicio) {
-            if (err) return res.sendStatus(404);
-            var nuevoPedido = {
-                cantidad_paquete: numeroPaquete,
-                costo_total: costoPedido,
-                direccion: direccion,
-                distrito: distrito,
-                tipoServicioId: objResult_servicio.id
-            };
-
-            Pedido.create(nuevoPedido, function(err, result_pedido) {
+        Cliente.findById(idCliente, function(err, objResult_cliente) {
+            console.log("****__*****",objResult_cliente);
+            TipoServicio.create(nuevoServicio, function(err, objResult_servicio) {
                 if (err) return res.sendStatus(404);
-                modo = true;
-                var mostrarTitulo = "Nuevo pedido";
-                var mostrarMensaje = "El pedido fue creado con exito";
-                Pedido.find({}, function(err, objResult_pedido) {
+                var nuevoPedido = {
+                    cantidad_paquete: numeroPaquete,
+                    costo_total: costoPedido,
+                    etapa_pedido: etapaPedido,
+                    direccion: direccion,
+                    distrito: distrito,
+                    tipoServicioId: objResult_servicio.id,
+                    clienteId: objResult_cliente.id
+                };
+                Pedido.create(nuevoPedido, function(err, result_pedido) {
                     if (err) return res.sendStatus(404);
-                    return res.render('pedidoPrincipal', {
-                        objResult_pedido: objResult_pedido,
-                        modo: modo,
-                        mostrarTitulo: mostrarTitulo,
-                        mostrarMensaje: mostrarMensaje
+                    modo = true;
+                    var mostrarTitulo = "Nuevo pedido";
+                    var mostrarMensaje = "El pedido para el cliente " + objResult_cliente.nombre + " fue creado con exito";
+                    Pedido.find({
+                        include: ['cliente']
+                    }, function(err, objResult_pedido) {
+                        if (err) return res.sendStatus(404);
+                        objResult_pedido = objResult_pedido.map(function(obj) {
+                            return obj.toJSON();
+                        });
+                        return res.render('pedidoPrincipal', {
+                            objResult_pedido: objResult_pedido,
+                            modo: modo,
+                            mostrarTitulo: mostrarTitulo,
+                            mostrarMensaje: mostrarMensaje
+                        });
                     });
                 });
             });
